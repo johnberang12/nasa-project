181c648acea8494386a13ef797a73408cb09254d
Johnny Berang <jober@Johnnys-iMac.local>
2022-04-10 00:48:42 +0900

    load planets in test

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,10 +1,16 @@
 const request = require('supertest');
 const app = require('../../app');
 const {mongoConnect} = require('../../../services/mongo');
+const { loadPlanetsData } = require('../../models/planets.model');
 
 describe('Launches API', ()=>{
     beforeAll(async()=>{
         await mongoConnect();
+        await loadPlanetsData();
+    });
+
+    afterAll(async ()=> {
+        await mongoDisconnect();
     });
 
     describe('Test GET /launches', ()=>{
